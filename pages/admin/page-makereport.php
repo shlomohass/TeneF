da3846cdadf4bf8c1c34b1298a03f2e51a68b994
shlomohass <shlomi@14tec.com>
2016-09-18 10:04:51 +0300

    added makereport table row filter with base gui operators.

--- pages/admin/page-makereport.php
+++ pages/admin/page-makereport.php
@@ -74,9 +74,14 @@ Trace::reg_var("all-am-status",$Page->variable("all-am-status"));
     <div class="col-sm-12">
         <?php Lang::P("page_makereport_newRep_title"); ?><span><?php echo date("d.m.Y"); ?></span>
         <div class="new_rep_container">
+            <form id="make_rep_edit_rep"class="form-inline">
+                <button type="button" class="btn btn-primary" onclick="">
+                          <?php Lang::P("page_makereport_but_edit_rep"); ?>
+                </button>
+            </form>
             <table class="new_rep_table">
                 <thead>
-                    <tr>
+                    <tr class="prevent-search">
                         <th>מס צ'</th>
                         <th>סוג</th>
                         <th>שייכות / ייעוד</th>
@@ -88,6 +93,84 @@ Trace::reg_var("all-am-status",$Page->variable("all-am-status"));
                     </tr>
                 </thead>
                 <tbody>
+                    <tr class="prevent-search">
+                        <td>
+                            <div class="form-group" style="max-width:95px;">
+                                <input type="text" class="form-control" id="tene-filter-amnum" placeholder="000000" />
+                            </div>
+                        </td>
+                        <td>
+                            <div class="form-group">
+                                <select class="form-control" id="tene-filter-amtype">
+                                    <option value="-1">ללא</option>
+                                    <?php
+                                        foreach ($Page->variable("all-am-groups") as $group) {
+                                            echo "<optgroup label='".$group["am_group_name"]."'>";
+                                            foreach ($Page->variable("all-am-types") as $type) {
+                                                if ($type["am_type_of_group"] === $group["am_group_id"]) {
+                                                    echo "<option value='".$type["am_type_id"]."'>".$type["am_type_name"]."</option>";
+                                                }
+                                            }
+                                            echo "</optgroup>";
+                                        }
+                                    ?>
+                                </select>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="tene-filter-yeud" placeholder="סנן יעוד" />
+                            </div>
+                        </td>
+                        <td>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="tene-filter-location" data-provide="typeahead" autocomplete="off" placeholder="סנן מיקום" />
+                            </div>
+                        </td>
+                        <td>
+                            <div class="form-group">
+                                <select class="form-control" id="tene-filter-amstatus">
+                                    <option value="-1">ללא</option>
+                                    <?php
+                                        foreach ($Page->variable("all-am-status") as $status) {
+                                            echo "<option value='".$status["am_status_name"]."'>".$status["am_status_name"]."</option>";
+                                        }
+                                    ?>
+                                </select>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="tene-filter-exp" placeholder="חפש פירוט" />
+                            </div>
+                        </td>
+                        <td>היסטוריית כשירות</td>
+                        <td>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="tene-filter-comment" placeholder="חפש הערות" />
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>654665</td>
+                        <td>דוד</td>
+                        <td>חפ"ק</td>
+                        <td>מפגש</td>
+                        <td>כשיר</td>
+                        <td>בעיית גיר והנעה</td>
+                        <td>היסטוריית כשירות</td>
+                        <td>הערות הולכות כאן</td>
+                    </tr>
+                    <tr>
+                        <td>654665</td>
+                        <td>דוד</td>
+                        <td>חפ"ק</td>
+                        <td>מפגש</td>
+                        <td>כשיר</td>
+                        <td>בעיית גיר והנעה</td>
+                        <td>היסטוריית כשירות</td>
+                        <td>הערות הולכות כאן</td>
+                    </tr>
                     <tr>
                         <td>654665</td>
                         <td>דוד</td>
@@ -182,8 +265,13 @@ Trace::reg_var("all-am-status",$Page->variable("all-am-status"));
                                 "<option value='" + response.results.repList[i].rep_group_num + "'>" + response.results.repList[i].rep_date + "</option>"
                             );
                         }
+                        
                         $prev.html($htmlPrev.join("")).select2();
+                        
                         //Load new:
+                        
+                        //Load inputs:
+                        window.teneReport.loadautocompletes();
                         
                         //Fade:
                         $(".makerep_loaded_unit_title").fadeIn(function(){
@@ -210,6 +298,37 @@ Trace::reg_var("all-am-status",$Page->variable("all-am-status"));
             $('#makereport_unit_set').prop("disabled", false);
             $(".makerep_prev_report, .makerep_new_report, .makerep_loaded_unit_title").fadeOut(function(){
             });
+        },
+        loadautocompletes : function() {
+            var source_amnum    = [],
+                source_yeud     = [],
+                source_location = [],
+                source_exp      = [],
+                source_com      = [];
+            $rows = $(".new_rep_table tr").not(".prevent-search");
+            $.each($rows, function(ind, ele) {
+                source_amnum.push($(ele).find("td").eq(0).text());
+                source_yeud.push($(ele).find("td").eq(2).text());
+                source_location.push($(ele).find("td").eq(3).text());
+                source_exp.push($(ele).find("td").eq(5).text());
+                source_com.push($(ele).find("td").eq(7).text());
+            });
+            var source_amnum_clean      = window.teneReport.arrayUnique(source_amnum);
+            var source_yeud_clean       = window.teneReport.arrayUnique(source_yeud);
+            var source_location_clean   = window.teneReport.arrayUnique(source_location);
+            var source_exp_clean        = window.teneReport.arrayUnique(source_exp);
+            var source_com_clean        = window.teneReport.arrayUnique(source_com);
+            $("#tene-filter-amnum").typeahead({ source:source_amnum_clean });
+            $("#tene-filter-yeud").typeahead({ source:source_yeud_clean });
+            $("#tene-filter-location").typeahead({ source:source_location_clean });
+            $("#tene-filter-exp").typeahead({ source:source_exp_clean });
+            $("#tene-filter-comment").typeahead({ source:source_com_clean });
+        },
+        arrayUnique : function(a) {
+            return a.reduce(function(p, c) {
+                if (p.indexOf(c) < 0) p.push(c);
+                return p;
+            }, []);
         }
      };
 </script>
